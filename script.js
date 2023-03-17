f58925228730d83f75859e809e9375c1c5dc7b19
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-03-17 15:03:41 +0200

    Add computer players functionality
    The visitors can choose to play against the computer player too.

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ const gameBoard = (() => {
     gameBoardArr[arrIndex] = playerParam.symbol;
   };
 
+  const takeComputerTurn = (playerParam) => {
+    const randomNumber = Math.floor(Math.random() * 9);
+    if (gameBoardArr[randomNumber] === undefined) {
+      gameBoardArr[randomNumber] = playerParam.symbol;
+      showGameBoard();
+    } else {
+      takeComputerTurn(playerParam);
+    }
+  };
+
   const checker = (arr, target) => target.every((v) => arr.includes(v));
 
   const checkWinner = (playerOneParam, playerTwoParam) => {
@@ -75,7 +85,14 @@ const gameBoard = (() => {
     showGameBoard();
   };
 
-  return { createGrid, showGameBoard, takeTurn, checkWinner, resetGrid };
+  return {
+    createGrid,
+    showGameBoard,
+    takeTurn,
+    checkWinner,
+    resetGrid,
+    takeComputerTurn,
+  };
 })();
 
 // Factory function for player creation
@@ -89,6 +106,8 @@ const displayController = (() => {
   const buttonsContainer = document.querySelector('.button-container');
   const humanOneButton = document.querySelector('.human1');
   const humanTwoButton = document.querySelector('.human2');
+  const aiOneButton = document.querySelector('.ai1');
+  const aiTwoButton = document.querySelector('.ai2');
   const startGameButton = document.querySelector('.start-game');
 
   const resetButton = document.querySelector('.reset-button');
@@ -102,15 +121,36 @@ const displayController = (() => {
     resetButton.style.display = 'none';
     winnerElement.style.display = 'none';
     playerTurnHeadline.textContent = 'Choose players';
+
     humanOneButton.addEventListener('click', () => {
+      if (aiOneButton.classList.contains('pressed')) {
+        aiOneButton.classList.remove('pressed');
+      }
       humanOneButton.classList.add('pressed');
       playerOne = playerFactory('Player 1', 'X');
     });
     humanTwoButton.addEventListener('click', () => {
+      if (aiTwoButton.classList.contains('pressed')) {
+        aiTwoButton.classList.remove('pressed');
+      }
       humanTwoButton.classList.add('pressed');
       playerTwo = playerFactory('Player 2', 'O');
     });
-    if ((playerOne, playerTwo)) {
+    aiOneButton.addEventListener('click', () => {
+      if (humanOneButton.classList.contains('pressed')) {
+        humanOneButton.classList.remove('pressed');
+      }
+      aiOneButton.classList.add('pressed');
+      playerOne = playerFactory('Computer 1', 'X');
+    });
+    aiTwoButton.addEventListener('click', () => {
+      if (humanTwoButton.classList.contains('pressed')) {
+        humanTwoButton.classList.remove('pressed');
+      }
+      aiTwoButton.classList.add('pressed');
+      playerTwo = playerFactory('Computer 2', 'O');
+    });
+    if (playerOne && playerTwo) {
       return true;
     }
   };
@@ -146,16 +186,30 @@ const displayController = (() => {
     for (let i = 0; i < cellNodes.length; i += 1) {
       // eslint-disable-next-line no-loop-func
       const listener = () => {
-        if (turn === 'playerOne') {
+        if (playerOne.name === 'Player 1' && playerTwo.name === 'Player 2') {
+          if (turn === 'playerOne') {
+            gameBoard.takeTurn(playerOne, i);
+            changeTurnHeadline(playerTwo, playerTurnHeadline);
+            turn = 'playerTwo';
+            gameBoard.showGameBoard();
+          } else if (turn === 'playerTwo') {
+            gameBoard.takeTurn(playerTwo, i);
+            changeTurnHeadline(playerOne, playerTurnHeadline);
+            turn = 'playerOne';
+            gameBoard.showGameBoard();
+          }
+        } else if (playerTwo.name === 'Computer 2') {
           gameBoard.takeTurn(playerOne, i);
-          changeTurnHeadline(playerTwo, playerTurnHeadline);
-          turn = 'playerTwo';
           gameBoard.showGameBoard();
-        } else {
+          if (gameBoard.checkWinner(playerOne, playerTwo) === false) {
+            gameBoard.takeComputerTurn(playerTwo);
+          }
+        } else if (playerOne.name === 'Computer 1') {
           gameBoard.takeTurn(playerTwo, i);
-          changeTurnHeadline(playerOne, playerTurnHeadline);
-          turn = 'playerOne';
           gameBoard.showGameBoard();
+          if (gameBoard.checkWinner(playerOne, playerTwo) === false) {
+            gameBoard.takeComputerTurn(playerOne);
+          }
         }
         switch (gameBoard.checkWinner(playerOne, playerTwo)) {
           case 'playerOneWins':
@@ -178,9 +232,19 @@ const displayController = (() => {
 
   startGameButton.addEventListener('click', () => {
     if (createPlayers()) {
-      buttonsContainer.style.display = 'none';
-      startGame();
-      playRound();
+      if (
+        aiOneButton.classList.contains('pressed') &&
+        aiTwoButton.classList.contains('pressed')
+      ) {
+        playerTurnHeadline.textContent = `Computers don't play against each other`;
+      } else {
+        buttonsContainer.style.display = 'none';
+        startGame();
+        if (playerOne.name === 'Computer 1') {
+          gameBoard.takeComputerTurn(playerOne);
+        }
+        playRound();
+      }
     }
   });
 
@@ -188,6 +252,9 @@ const displayController = (() => {
     gameBoard.resetGrid();
     winnerElement.style.display = 'none';
     turn = 'playerOne';
+    if (playerOne.name === 'Computer 1') {
+      gameBoard.takeComputerTurn(playerOne);
+    }
     playRound();
     resetButton.style.display = 'none';
   });
